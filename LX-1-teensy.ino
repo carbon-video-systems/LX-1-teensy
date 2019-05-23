7140da6a816e6e5cd93c85aadc671609a102b59c
Anthony Duen <anthony.duen@gmail.com>
2019-05-22 17:21:11 -0700

    motor calibration framework

--- LX-1-teensy.ino
+++ LX-1-teensy.ino
@@ -1,31 +1,61 @@
+/* Includes ---------------------------------------------------------------------------------------*/
 #include "ODriveLib.h"
+#include "calibration.h"
+
+#include <Arduino.h>
+
+/* Constants --------------------------------------------------------------------------------------*/
+#define TESTING
+#define odrive_serial   Serial2
+#define numMotors       2
 
 // Printing with stream operator
 template<class T> inline Print& operator <<(Print &obj,     T arg) { obj.print(arg);    return obj; }
 template<>        inline Print& operator <<(Print &obj, float arg) { obj.print(arg, 4); return obj; }
 
-// Variables
-#define TESTING
-#define odrive_serial Serial1
-
-
-// ODrive object
+/* Variables --------------------------------------------------------------------------------------*/
 ODriveClass odrive(odrive_serial);
 
+/* Functions --------------------------------------------------------------------------------------*/
+/**
+ * @brief   The Arduino setup function
+ * @param   None
+ * @return  None
+ */
 void setup() {
-  // ODrive uses 115200 baud
-  odrive_serial.begin(115200);
-  while(!odrive_serial);
+    // ODrive uses 115200 baud
+    odrive_serial.begin(115200);
+    while(!odrive_serial);
 
-  #ifdef TESTING
-    // Serial to PC
-    Serial.begin(115200);
-    while (!Serial); // wait for Arduino Serial Monitor to open]
-  #endif
-  delay(100);
+    #ifdef TESTING
+        // Serial to PC
+        Serial.begin(115200);
+        while (!Serial); // wait for Arduino Serial Monitor to open]
+    #endif
+
+    int32_t current_state;
+    // search for post-startup ODrive
+    for (int axis = 0; axis < numMotors; axis++) {
+        current_state = odrive.readState(axis);
+        while(current_state != ODriveClass::AXIS_STATE_CLOSED_LOOP_CONTROL || current_state != ODriveClass::AXIS_STATE_IDLE){
+            delay(100);
+            current_state = odrive.readState(axis);
+        }
+    }
+
+    while (odrive.readState(0) != ODriveClass::AXIS_STATE_CLOSED_LOOP_CONTROL || ){
+
+    }
+    // calibration check + sequence
+
+    delay(100);
 }
 
+/**
+ * @brief   The Arduino loop function
+ * @param   None
+ * @return  None
+ */
 void loop() {
-  // put your main code here, to run repeatedly:
-  delay(500);
+    delay(500);
 }
---
+++ calibration.h
@@ -0,0 +1,12 @@
+/* Calibration Header */
+#ifndef Calibration_h
+#define Calibration_h
+
+/* Includes --------------------------------------------------------*/
+#include "Arduino.h"
+#include "ODriveLib.h"
+
+#define hello   10
+
+
+#endif //Calibration_h
